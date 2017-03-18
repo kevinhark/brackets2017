4a12fe0171e8081c0f413c629a3760e37244f057
Kevin Harkins <kevinharkins@Kevins-MacBook-Pro.local>
2017-03-17 23:27:39 -0500

    timestamping my picks for r32

---
+++ r32.py
@@ -0,0 +1,115 @@
+
+weight = 6
+name = "Round of 32"
+
+winners = (
+    # South
+    'Villanova', 'Wisconsin',
+    'Virginia', 'Florida',
+    'USC', 'Baylor',
+    'South Carolina', 'Duke',
+
+    # West
+    'Gonzaga', 'Northwestern',
+    'Notre Dame', 'West Virginia',
+    'Xavier', 'Florida St',
+    'St Marys', 'Arizona',
+
+    # East
+    'Kansas', 'Michigan St',
+    'Iowa St', 'Purdue',
+    'Rhode Island', 'Oregon',
+    'Michigan', 'Louisville',
+
+    # Midwest
+    'UNC', 'Arkansas',
+    'MTSU', 'Butler',
+    'Cincinnati', 'UCLA',
+    'Wichita St', 'Kentucky',
+)
+
+
+picks= {
+
+'Chris':
+(
+     'Villanova', 'Wisconsin', 'UNC Wilmington', 'Florida', 'SMU', 'Baylor', 'South Carolina', 'Duke',
+     'Gonzaga', 'Vanderbilt', 'Notre Dame', 'West Virginia', 'Xavier', 'Florida St', 'VCU','Arizona',
+     'Kansas', 'Michigan St', 'Iowa St', 'Purdue', 'Creighton', 'Oregon', 'Michigan', 'Louisville',
+     'UNC',  'Arkansas', 'Minnesota', 'Butler', 'Cincinnati', 'UCLA', 'Dayton', 'Kentucky',
+ ),
+
+'Justin':
+(
+    'Villanova', 'Wisconsin', 'Virginia', 'E Tenn St', 'SMU', 'Baylor', 'Marquette', 'Duke',
+    'Gonzaga', 'Vanderbilt', 'Notre Dame', 'Bucknell', 'Xavier', 'Florida St', 'St Marys','Arizona',
+    'Kansas', 'Miami', 'Nevada', 'Vermont', 'Rhode Island', 'Oregon', 'Oklahoma St', 'Louisville',
+    'UNC',  'Seton Hall', 'MTSU', 'Butler', 'Cincinnati', 'Kent St', 'Wichita St', 'Kentucky',
+),
+
+'Jerry':
+(
+    'Villanova', 'Wisconsin', 'Virginia', 'Florida', 'USC', 'Baylor', 'Marquette','Duke',
+    'Gonzaga', 'Vanderbilt', 'Notre Dame', 'Bucknell', 'Xavier', 'Florida St', 'VCU', 'Arizona',
+    'Kansas', 'Michigan St', 'Iowa St', 'Purdue', 'Rhode Island', 'Oregon', 'Michigan', 'Louisville',
+    'UNC', 'Seton Hall', 'Minnesota', 'Winthrop', 'Cincinnati', 'UCLA', 'Dayton', 'Kentucky',
+),
+
+'Jessica':
+(
+     'Villanova','Virginia','Baylor','Duke',
+     'Gonzaga','Norte Dame','Xavier','Arizona',
+     'Kansas','Purdue','Oregon','Louisville',
+     'UNC','MTSU','UCLA','Kentucky',
+),
+
+'Owen':
+(
+     'Villanova', 'Virginia', 'Baylor', 'Duke',
+     'Gonzaga', 'West Virginia', 'Florida St', 'Arizona',
+     'Kansas', 'Iowa state', 'Oregon', 'Michigan',
+     'UNC', 'Butler', 'UCLA', 'Kentucky',
+),
+
+'Victor':
+(
+    'Villanova', 'Virginia Tech', 'Virginia', 'Florida', 'USC', 'Baylor', 'South Carolina', 'Duke',
+    'Gonzaga', 'Vanderbilt', 'Notre Dame', 'West Virginia', 'Maryland', 'Florida St', 'VCU', 'Arizona',
+    'Kansas', 'Michigan St', 'Iowa St', 'Purdue', 'Rhode Island', 'Oregon', 'Oklahoma St', 'Louisville',
+    'UNC', 'Arkansas', 'MTSU', 'Butler', 'Cincinnati', 'UCLA', 'Wichita St', 'Kentucky',
+),
+
+'Kristin':
+(
+     'Villanova', 'Virginia Tech', 'Virginia', 'E Tenn St', 'SMU', 'Baylor', 'Marquette', 'Duke',
+     'Gonzaga', 'Vanderbilt', 'Notre Dame', 'West Virginia', 'Maryland', 'Florida St', 'VCU', 'Arizona',
+     'Kansas', 'Michigan St', 'Iowa St', 'Vermont', 'Creighton', 'Oregon', 'Michigan', 'Louisville',
+     'UNC', 'Arkansas', 'Minnesota', 'Butler', 'Cincinnati', 'Kent St', 'Dayton', 'Kentucky',
+),
+
+'Suzie':
+(
+     'Villanova', 'Wisconsin', 'Virginia', 'Florida', 'SMU', 'Baylor', 'Marquette', 'Duke',
+     'Gonzaga', 'Vanderbilt', 'Notre Dame', 'Bucknell', 'Maryland', 'Florida St', 'VCU', 'Arizona',
+     'Kansas', 'Michigan St', 'Iowa St', 'Purdue', 'Rhode Island', 'Oregon', 'Michigan', 'Louisville',
+     'UNC', 'Seton Hall', 'Minnesota', 'Butler', 'Cincinnati', 'UCLA', 'Wichita St', 'Kentucky',
+),
+
+'Ryan':
+(
+    'Villanova', 'Wisconsin', 'Virginia', 'Florida', 'SMU', 'Baylor', 'South Carolina', 'Duke',
+    'Gonzaga', 'Vanderbilt', 'Notre Dame', 'West Virginia', 'Maryland', 'Florida St', 'VCU','Arizona',
+    'Kansas', 'Miami', 'Nevada', 'Vermont', 'Rhode Island', 'Oregon', 'Michigan', 'Louisville',
+    'UNC', 'Arkansas', 'MTSU', 'Butler', 'Cincinnati', 'UCLA', 'Wichita St', 'Kentucky',
+),
+
+'Kevin':
+(
+    'Villanova', 'Florida', 'Baylor', 'Duke',
+    'Gonzaga', 'West Virginia', 'Xavier', 'Arizona',
+    'Kansas', 'Iowa St', 'Oregon', 'Louisville',
+    'UNC', 'MTSU', 'UCLA', 'Kentucky',
+),
+
+
+}
