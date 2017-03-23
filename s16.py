21bbc1219fe5b0044bc95905d433d4b394a2740d
Kevin Harkins <kevinharkins@kevins-imac.dhcp.mc.vanderbilt.edu>
2017-03-23 11:43:16 -0500

    sweet 16 picks are in

---
+++ s16.py
@@ -0,0 +1,107 @@
+
+weight = 20
+name = "Sweet 16"
+
+winners = (
+    # South
+    None, #'Wisconsin', 'Florida',
+    None, #'Baylor', 'South Carolina',
+
+    # West
+    None, #'Gonzaga', 'West Virginia',
+    None, #'Xavier', 'Arizona',
+
+    # East
+    None, #'Kansas', 'Purdue',
+    None, #'Oregon', 'Michigan',
+
+    # Midwest
+    None, #'UNC', 'Butler',
+    None, #'UCLA', 'Kentucky',
+)
+
+
+picks= {
+
+'Chris':
+(
+    'Florida', 'South Carolina',
+    'West Virginia', 'Arizona',
+    'Kansas', 'Oregon',
+    'UNC', 'UCLA',
+),
+
+'Justin':
+(
+    'Florida','Baylor',
+    'West Virginia', 'Arizona',
+    'Kansas', 'Michigan',
+    'UNC', 'Kentucky',
+),
+
+'Jerry':
+(
+    'Florida', 'Baylor',
+    'Gonzaga','Arizona',
+    'Kansas', 'Michigan',
+    'UNC','UCLA',
+),
+
+'Jessica':
+(
+     'Wisconsin','Baylor',
+     'Gonzaga','Arizona',
+     'Kansas','Michigan',
+     'UNC','UCLA',
+),
+
+'Owen':
+(
+     'Wisconsin', 'Baylor',
+     'Gonzaga', 'Arizona',
+     'Purdue', 'Oregon',
+     'Butler', 'UCLA',
+),
+
+'Victor':
+(
+    'Wisconsin','South Carolina',
+    'Gonzaga','Arizona',
+    'Kansas','Michigan',
+    'UNC','Kentucky',
+),
+
+'Kristin':
+(
+    'Wisconsin','Baylor',
+    'Gonzaga', 'Arizona',
+    'Kansas','Michigan',
+    'Butler', 'UCLA',
+),
+
+'Suzie':
+(
+    'Florida', 'Baylor',
+    'West Virginia', 'Arizona',
+    'Kansas', 'Michigan',
+    'UNC', 'Kentucky',
+),
+
+'Ryan':
+(
+    'Florida', 'Baylor',
+    'Gonzaga', 'Arizona',
+    'Kansas', 'Michigan',
+    'Butler', 'Kentucky',
+),
+
+'Kevin':
+(
+    'Florida', 'Baylor',
+    'West Virginia', 'Arizona',
+    'Kansas', 'Michigan',
+    'UNC', 'UCLA',
+),
+
+
+}
