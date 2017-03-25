0f9c319755559637c3578b2b0806d5abcc63c214
Kevin Harkins <kevinharkins@Kevins-MacBook-Pro.local>
2017-03-25 15:49:50 -0500

    e8 picks

---
+++ e8.py
@@ -0,0 +1,103 @@
+
+weight = 40
+name = "Elite 8"
+
+winners = (
+    # South
+    None, #'Florida', 'South Carolina',
+
+    # West
+    None, #'Gonzaga', 'Xavier',
+
+    # East
+    None, #'Kansas', 'Oregon',
+
+    # Midwest
+    None, #'UNC', 'Kentucky',
+)
+
+
+picks= {
+
+'Chris':
+(
+    'Florida',
+    'Gonzaga',
+    'Oregon',
+    'UNC',
+),
+
+'Justin':
+(
+    'South Carolina',
+    'Xavier',
+    'Oregon',
+    'Kentucky',
+),
+
+'Jerry':
+(
+    'South Carolina',
+    'Xavier',
+    'Kansas',
+    'UNC',
+),
+
+'Jessica':
+(
+    'South Carolina',
+    'Gonzaga',
+    'Oregon',
+    'Kentucky',
+),
+
+'Owen':
+(
+    'South Carolina',
+    'Xavier',
+    'Oregon',
+    'Kentucky',
+),
+
+'Victor':
+(
+    'Florida',
+    'Gonzaga',
+    'Kansas',
+    'Kentucky',
+),
+
+'Kristin':
+(
+    'South Carolina',
+    'Gonzaga',
+    'Kansas',
+    'UNC',
+),
+
+'Suzie':
+(
+    'Florida',
+    'Gonzaga',
+    'Kansas',
+    'Kentucky',
+),
+
+'Ryan':
+(
+    'Florida',
+    'Gonzaga',
+    'Kansas',
+    'Kentucky',
+),
+
+'Kevin':
+(
+    'South Carolina',
+    'Gonzaga',
+    'Kansas',
+    'UNC',
+),
+
+
+}
