bde3d61c2bf8cb02ac95911d46160117632c2d0a
Kevin Harkins <kevinharkins@kevins-imac.dhcp.mc.vanderbilt.edu>
2017-03-31 12:47:11 -0500

    f4 picks

---
+++ f4.py
@@ -0,0 +1,74 @@
+
+weight = 80
+name = "Final 4"
+
+winners = (
+    None, #'South Carolina','Gonzaga',
+    None, #'Oregon','UNC',
+)
+
+
+picks= {
+
+'Chris':
+(
+    'Gonzaga',
+    'UNC',
+),
+
+'Justin':
+(
+    'South Carolina',
+    'UNC'
+),
+
+'Jerry':
+(
+    'Gonzaga',
+    'Oregon',
+),
+
+'Jessica':
+(
+    'Gonzaga',
+    'Oregon',
+),
+
+'Owen':
+(
+    'Gonzaga',
+    'UNC',
+),
+
+'Victor':
+(
+    'South Carolina',
+    'UNC',
+),
+
+'Kristin':
+(
+    'Gonzaga',
+    'Oregon',
+),
+
+'Suzie':
+(
+    'South Carolina',
+    'Oregon',
+),
+
+'Ryan':
+(
+    'Gonzaga',
+    'UNC',
+),
+
+'Kevin':
+(
+    'Gonzaga',
+    'Oregon',
+),
+
+
+}
